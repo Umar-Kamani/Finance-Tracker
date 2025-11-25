0e394fe0cb2ade47cd3245ba3ebd81a67956483d
Umar Kamani <umar.kamani@gmail.com>
2025-11-25 22:47:18 +0400

    Integrated classes from financy_classes.py. Modified add_income and add_expense function to work with classes. Added per_category_spending_history function

--- Financy - V1.py
+++ Financy - V1.py
@@ -1,9 +1,10 @@
 #Global Variables
 from datetime import date
 from tabulate import tabulate
+import financy_classes
+
 
 balance = 0.00  #This is the balance variable for the program, all income and expense will be deducted from here.
-transactions =[] #This list stores all transactions that happen in the program, whether it is an income or expense
 
 def login(): #This function is used for simple user authentication
     user = { #This dictionary stores the lists of users that are allowed to log into the Financy System
@@ -55,17 +56,19 @@ def income_menu(): # This is the income submenu, it is where all the options reg
     elif income_choice == '2':
         print("---------------")
         show_income_table() #redirects to this function
-    elif income_choice == '3':
+    elif income_choice == '3' or income_choice == 'exit':
         print("Returning to Main Menu")
     return main_menu()
 ################################################################################################
 def expense_menu():
     print("---------------")
+    print("Welcome to the Expense Menu")
+    print("---------------")
     print("1. Add Expense")
     print("2. View Expense History")
     print("3. Exit to Main Menu")
     while True: #This loop checks for correct user choice input
-        expense_choice = input("Enter your choice: ")
+        expense_choice = input("Enter your choice: ").lower()
         if expense_choice not in ('1', '2', '3', 'exit'):
             print("Invalid choice. Please try again.")
         else:
@@ -76,12 +79,14 @@ def expense_menu():
     elif expense_choice == '2':
         print("---------------")
         show_expense_table()
-    elif expense_choice == '3':
+    elif expense_choice == '3' or expense_choice == 'exit':
         print("Returning to Main Menu")
     return main_menu()
 ################################################################################################
 def account_analytics_menu():
     global balance
+    print("---------------")
+    print("Welcome to the Account Analytics Menu")
     print("---------------")
     print(f"Current balance: ${balance}")
     print("1. View Total Income History")
@@ -91,13 +96,13 @@ def account_analytics_menu():
     print("5. Exit to Main Menu")
     while True: #This loop checks for correct user choice input
         account_analytics_choice = input("Enter your choice: ")
-        if account_analytics_choice not in ('1', '2', '3', '4', '5' 'exit'):
+        if account_analytics_choice not in ('1', '2', '3', '4', '5', 'exit'):
             print("Invalid choice. Please try again.")
         else:
             break
     if account_analytics_choice == '1':
         print("---------------")
-        show_income_table()
+        show_income_table(Analytics=True)
     elif account_analytics_choice == '2':
         print("---------------")
         show_expense_table()
@@ -106,10 +111,13 @@ def account_analytics_menu():
         print(f"Your current balance: ${balance}")
     elif account_analytics_choice == '4':
         print("---------------")
-        spending_cat_breakdown("Food")
-        spending_cat_breakdown("Rent")
-
-    else:
+        per_category_spending_history("Food")
+        per_category_spending_history("Rent")
+        per_category_spending_history("Entertainment")
+        per_category_spending_history("Clothing")
+        per_category_spending_history("Loan")
+        per_category_spending_history("Other")
+    elif account_analytics_choice == '5' or account_analytics_choice == 'exit':
         print("Returning to Main Menu.")
     return main_menu()
 ################################################################################################
@@ -127,84 +135,104 @@ def menu_selector(main_menu_choice):
 def add_income():
     global balance
     print(f"Current balance: ${balance}")
-    income = input("Enter your income ($): ")
-    while not income.isdigit():
-        print("Invalid income. Please try again. Please enter only numbers.")
-        income = input("Enter your income ($): ")
-    else: income = float(income)
+    while True:
+        income_amount = input("Enter your income ($): ")
+        try:
+            income_amount = float(income_amount)
+            if income_amount <= 0:
+                print("Invalid income. Please try again. Please enter only numbers.")
+            else:
+                break
+        except ValueError:
+            print("Invalid income. Please try again. Please enter only numbers.")
     income_date = date.today().strftime("%d-%m-%Y")
-    income_entry = {
-        "Type": "Income",
-        "Amount": f"${income}",
-        "Date": income_date
-    }
-    transactions.append(income_entry)
-    balance += income
+    income_description = input("Enter your income description: ")
+    new_income = financy_classes.Income("Income", income_amount,"Income", income_description, income_date)
+    balance += income_amount
     print(f"Your new balance is ${balance}")
     return income_menu()
 ###################################################################################################
 def add_expense():
     global balance
+    valid_expenses = ['Food', 'Rent', 'Entertainment', 'Clothing', 'Loan', 'Other']
     print(f"Current balance: ${balance}")
-    expense = input("Enter your expense ($): ")
-    while not expense.isdigit():
-        print("Invalid expense. Please try again. Please enter only numbers.")
-        expense = input("Enter your expense ($): ")
-    else: expense = float(expense)
-    expense_description = input("Enter your expense description: ")
-    expense_category = input("Enter your expense category: ").capitalize()
-    expense_category_list = ['Food', 'Rent', 'Entertainment', 'Clothing', 'Loan']
-    header = "Expense Categories"
-    expense_date = date.today().strftime("%d-%m-%Y")
-    while expense_category not in expense_category_list:
-            print(f"Invalid choice. Please enter a valid category such as Food, Rent, Entertainment, Clothing, Loan.")
-            expense_category = input("Enter your expense category: ")
+    while True:
+        expense_amount = input("Enter your expense ($): ")
+        try:
+            expense_amount = float(expense_amount)
+            if expense_amount <= 0:
+                print("Invalid expense. Please try again. Please enter only numbers.")
+            else:
+                break
+        except ValueError:
+            print("Invalid expense. Please try again. Please enter only numbers.")
 
-    expense_entry = {
-        "Type": "Expense",
-        "Amount": f"${expense}",
-        "Description": expense_description,
-        "Category": expense_category,
-        "Date": expense_date
-    }
-    transactions.append(expense_entry)
-    balance -= expense
+    expense_date = date.today().strftime("%d-%m-%Y")
+    expense_description = input("Enter your expense description: ")
+
+    while True:
+        expense_category = input("Enter your expense category: ").capitalize()
+        if expense_category not in valid_expenses:
+            print("Invalid choice. Please enter a valid category such as Food, Rent, Entertainment, Clothing, Loan and Other")
+        else:
+            break
+
+    new_expense = financy_classes.Expenses("Expenses", expense_amount,expense_category, expense_description, expense_date)
+    balance -= expense_amount
     print(f"Your new balance is ${balance}")
     return expense_menu()
 ####################################################################################################
-def show_income_table():
-    incomes = []
-    for t in transactions:
-        if t["Type"] == "Income":
-            incomes.append(t)
-    tabulate_income_data = incomes
-    print(tabulate(incomes, headers="keys", tablefmt="fancy_grid"))
-    return income_menu()
+def show_income_table(Analytics):
+    incomes = [t for t in financy_classes.Transactions.transactions_registry if t.ttype == "Income"]
+    income_table = []
+    for t in incomes:
+        income_table.append({
+            "Type": "Income",
+            "Amount ($)": t.amount,
+            "Category": "Income",
+            "Description": t.description,
+            "Date": t.date
+        })
+    print(tabulate(income_table, headers="keys", tablefmt="fancy_grid"))
+
+    if Analytics:
+        return account_analytics_menu()
+    else:
+        return income_menu()
 #####################################################################################################
-def show_expense_table():
-    expenses = []
-    for t in transactions:
-        if t["Type"] == "Expense":
-            expenses.append(t)
-    tabulate_expense_data = expenses
-    print(tabulate(expenses, headers="keys", tablefmt="fancy_grid"))
-    return expense_menu()
+def show_expense_table(Analytics):
+    expenses = [t for t in financy_classes.Transactions.transactions_registry if t.ttype == "Expense"]
+    expense_table = []
+    for t in expenses:
+        expense_table.append({
+            "Type": "Expense",
+            "Amount ($)": t.amount,
+            "Category": t.category,
+            "Description": t.description,
+            "Date": t.date
+        })
+    print(tabulate(expense_table, headers="keys", tablefmt="fancy_grid"))
+    if Analytics:
+        return account_analytics_menu()
+    else:
+        return expense_menu()
 ####################################################################################################
-def spending_cat_breakdown(food):
-    food = []
-    for t in transactions:
-        if t["Category"] == "Food":
-            food.append(t)
-    print(tabulate(food, headers="keys", tablefmt="fancy_grid"))
-    return account_analytics_menu()
-######################################################################################################
-def spending_cat_breakdown(Rent):
-    Rent = []
-    for t in transactions:
-        if t["Category"] == "Rent":
-            Rent.append(t)
-    print(tabulate(Rent, headers="keys", tablefmt="fancy_grid"))
-    return account_analytics_menu()
+def per_category_spending_history(category):
+    sp_category =[t for t in financy_classes.Transactions.transactions_registry if t.category == category]
+
+    sp_category_history = []
+    for t in sp_category:
+        sp_category_history.append({
+            "Type": t.ttype,
+            "Amount ($)": t.amount,
+            "Category": t.category,
+            "Description": t.description,
+            "Date": t.date
+        })
+    print(f"Please find your spending history for {category} below.")
+    print("____________________________________________________________________")
+    print(tabulate(sp_category_history, headers="keys", tablefmt="fancy_grid"))
+###################################################################################################
 
 
 login()
