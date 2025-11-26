6f2f028c966521fee4db5148e0a51e871d449246
Umar Kamani <umar.kamani@gmail.com>
2025-11-27 00:17:12 +0400

    Added some code so that each transaction has an ID

--- financy_classes.py
+++ financy_classes.py
@@ -1,7 +1,9 @@
 
 class Transactions:
     transactions_registry = []
+    transactions_id = 0
     def __init__(self, ttype, amount, category, description, date):
+        self.transactions_id = Transactions.transactions_id + 1
         self.ttype = ttype
         self.amount = float(amount)
         self.category = category
